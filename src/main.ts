9cbce771d1daaf517168412a1910eb253a4b649a
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-26 14:43:48 +0200

    feat: add api reader

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { join } from 'path';
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const apiPath = join(__dirname, '..', 'doc/api.yaml');
+  const apiYaml = await readFile(apiPath, 'utf-8');
+
+  const config: OpenAPIObject = load(apiYaml) as OpenAPIObject;
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(4000);
+  console.log('Server starts at http://localhost:4000');
+  console.log('Docs at http://localhost:4000/docs');
 }
 bootstrap();
