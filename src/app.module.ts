eaaf591ffc9e27d82e701d1d7aa7bf5596a6fad7
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-30 16:18:40 +0200

    feat: generate artists resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ArtistsModule } from './artists/artists.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ArtistsModule],
   controllers: [AppController],
   providers: [AppService],
 })
