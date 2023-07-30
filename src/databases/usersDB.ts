09c3db506869a5a4199e4ce634f74b84ca37ae0a
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-30 12:26:53 +0200

    feat: add userDB

---
+++ src/databases/usersDB.ts
@@ -0,0 +1,14 @@
+import { IUser } from 'src/interfaces/IUser';
+
+const userDB: IUser[] = [
+  {
+    id: '1',
+    createdAt: 1,
+    login: '1',
+    password: '1',
+    updatedAt: 1,
+    version: 1,
+  },
+];
+
+export default userDB;
