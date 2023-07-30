395503ee8c90c34ba3b684a0a848ddb92a5c793f
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-30 23:06:06 +0200

    feat: add decorators for favorites

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -3,43 +3,241 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { CreateFavoriteDto } from './dto/create-favorite.dto';
 import { UpdateFavoriteDto } from './dto/update-favorite.dto';
+import {
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { ResponsesMessages } from 'src/interfaces/ResponsesMessages';
+import exceptionHandler from 'src/utils/exceptionHandler';
 
+@ApiTags('Favorites')
 @Controller('favs')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
-  @Post()
-  create(@Body() createFavoriteDto: CreateFavoriteDto) {
-    return this.favoritesService.create(createFavoriteDto);
-  }
-
   @Get()
-  findAll() {
-    return this.favoritesService.findAll();
+  @ApiOperation({
+    summary: 'Get all favorites',
+    description: 'Gets all favorites movies, tracks and books',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful operation',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: { $ref: getSchemaPath('Favorites') },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: ResponsesMessages.UnauthorizedError,
+  })
+  @HttpCode(200)
+  getFavorites() {
+    try {
+      return this.favoritesService.getFavorites();
+    } catch (error) {
+      exceptionHandler(error as Error);
+    }
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.favoritesService.findOne(+id);
+  @Post('/track/:trackId')
+  @ApiParam({ name: 'trackId', type: 'string', format: 'uuid' })
+  @ApiOperation({
+    summary: 'Add track to the favorites',
+    description: 'Add track to the favorites',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Added successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad. trackId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 401,
+    description: ResponsesMessages.UnauthorizedError,
+  })
+  @ApiResponse({
+    status: 422,
+    description: "Track with id doesn't exist.",
+  })
+  @HttpCode(201)
+  postFavoritesTrack() {
+    try {
+      return this.favoritesService.postFavoritesTrack();
+    } catch (error) {
+      exceptionHandler(error as Error);
+    }
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateFavoriteDto: UpdateFavoriteDto,
-  ) {
-    return this.favoritesService.update(+id, updateFavoriteDto);
+  @Delete('/track/:trackId')
+  @ApiParam({ name: 'trackId', type: 'string', format: 'uuid' })
+  @ApiOperation({
+    summary: 'Delete track from favorites',
+    description: 'Delete track from favorites',
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'Deleted successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad. trackId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 401,
+    description: ResponsesMessages.UnauthorizedError,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Track was not found',
+  })
+  @HttpCode(204)
+  deleteFavoritesTrack() {
+    try {
+      return this.favoritesService.deleteFavoritesTrack();
+    } catch (error) {
+      exceptionHandler(error as Error);
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.favoritesService.remove(+id);
+  @Post('/album/:albumId')
+  @ApiParam({ name: 'albumId', type: 'string', format: 'uuid' })
+  @ApiOperation({
+    summary: 'Add album to the favorites',
+    description: 'Add album to the favorites',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Added successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad. albumId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 401,
+    description: ResponsesMessages.UnauthorizedError,
+  })
+  @ApiResponse({
+    status: 422,
+    description: "Album with id doesn't exist.",
+  })
+  @HttpCode(201)
+  postFavoritesAlbum() {
+    try {
+      return this.favoritesService.postFavoritesAlbum();
+    } catch (error) {
+      exceptionHandler(error as Error);
+    }
+  }
+
+  @Delete('/album/:albumId')
+  @ApiParam({ name: 'albumId', type: 'string', format: 'uuid' })
+  @ApiOperation({
+    summary: 'Delete album from favorites',
+    description: 'Delete album from favorites',
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'Deleted successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad. albumId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 401,
+    description: ResponsesMessages.UnauthorizedError,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Album was not found',
+  })
+  @HttpCode(204)
+  deleteFavoritesAlbum() {
+    try {
+      return this.favoritesService.deleteFavoritesAlbum();
+    } catch (error) {
+      exceptionHandler(error as Error);
+    }
+  }
+
+  @Post('/artist/:artistId')
+  @ApiParam({ name: 'artistId', type: 'string', format: 'uuid' })
+  @ApiOperation({
+    summary: 'Add artist to the favorites',
+    description: 'Add artist to the favorites',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Added successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. artistId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 401,
+    description: ResponsesMessages.UnauthorizedError,
+  })
+  @ApiResponse({
+    status: 422,
+    description: "Artist with id doesn't exist.",
+  })
+  @HttpCode(201)
+  postFavoritesArtist() {
+    try {
+      return this.favoritesService.postFavoritesAlbum();
+    } catch (error) {
+      exceptionHandler(error as Error);
+    }
+  }
+
+  @Delete('/artist/:artistId')
+  @ApiParam({ name: 'artistId', type: 'string', format: 'uuid' })
+  @ApiOperation({
+    summary: 'Delete artist from favorites',
+    description: 'Delete artist from favorites',
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'Deleted successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. artistId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 401,
+    description: ResponsesMessages.UnauthorizedError,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Artist was not found',
+  })
+  @HttpCode(204)
+  deleteFavoritesArtist() {
+    try {
+      return this.favoritesService.deleteFavoritesAlbum();
+    } catch (error) {
+      exceptionHandler(error as Error);
+    }
   }
 }
