bd5f216b7208c08d158a08c3f2e3722e7db2a3b4
Aliaksandr <sanya55bychyk@gmail.com>
2023-08-21 11:53:28 +0200

    feat: add LoggingMiddleware

---
+++ src/logging/logging.middleware.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { LoggingService } from './logging.service';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  constructor(private readonly loggingService: LoggingService) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, body, originalUrl, query } = req;
+    const { statusCode } = res;
+    const date = new Date();
+    const currentTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    this.loggingService.logRequest(
+      `[${currentTime}] (${method}:${originalUrl}) Body: ${JSON.stringify(
+        body,
+      )}, Query: ${JSON.stringify(query)}, StatusCode: ${statusCode}.`,
+    );
+    next();
+  }
+}
